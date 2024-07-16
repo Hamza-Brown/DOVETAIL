e17fb6fbdd42938b6e028e345d31d373433d939f
Hamza-Brown <hamzabrown1478@gmail.com>
2024-07-16 22:22:39 +0500

    Add files via upload

---
+++ main javaScript.js
@@ -0,0 +1,11 @@
+
+
+                function showSiebar(){
+                    const sidebar = document.querySelector('.sidebar')
+                    sidebar.style.display = 'flex'
+                }
+                function hideSidebar(){
+                    const sidebar = document.querySelector('.sidebar')
+                    sidebar.style.display = 'none'
+                }
+            
